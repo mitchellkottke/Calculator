9c007a31fa48e32d0999a0c9c91264683e674f27
Mitchell Kottke <mkottke2000@gmail.com>
2024-12-26 01:44:06 -0600

    Rewrite stack to original code, remove and ignore executables

--- stack/stack.go
+++ stack/stack.go
@@ -1,51 +1,44 @@
-//Credit to https://github.com/golang-collections/collections and calebdoxsey
+// Package Stack is a standard stack
 
 package stack
 
 type (
-	Stack struct {
-		top    *node
-		length int
-	}
 	node struct {
-		value interface{}
-		prev  *node
+		next  *node
+		value any
+	}
+
+	Stack struct {
+		head *node
 	}
 )
 
-// Create a new stack
+// Create an empty stack
 func New() *Stack {
-	return &Stack{nil, 0}
+	return &Stack{head: nil}
 }
 
-// Return the number of items in the stack
-func (this *Stack) Len() int {
-	return this.length
+// Push an item on the stack
+func (stack *Stack) Push(item any) {
+	stack.head = &node{next: stack.head, value: item}
 }
 
-// View the top item on the stack
-func (this *Stack) Peek() interface{} {
-	if this.length == 0 {
-		return nil
-	}
-	return this.top.value
-}
-
-// Pop the top item of the stack and return it
-func (this *Stack) Pop() interface{} {
-	if this.length == 0 {
+// Pop an item off the stack
+func (stack *Stack) Pop() any {
+	if stack.head == nil {
 		return nil
 	}
 
-	n := this.top
-	this.top = n.prev
-	this.length--
-	return n.value
+	value := stack.head.value
+	stack.head = stack.head.next
+	return value
 }
 
-// Push a value onto the top of the stack
-func (this *Stack) Push(value interface{}) {
-	n := &node{value, this.top}
-	this.top = n
-	this.length++
+// Look at the top item of the stack
+func (stack *Stack) Peek() any {
+	if stack.head == nil {
+		return nil
+	}
+
+	return stack.head.value
 }
