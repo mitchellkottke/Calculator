a111147251be6a8fe3ebab82d87fcd9d6c5161fe
Mitchell Kottke <mkottke2000@gmail.com>
2024-12-29 00:30:23 -0600

    Add barebone UI

--- main.go
+++ main.go
@@ -12,32 +12,78 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
-	"strings"
 
 	"calculator/calculations"
+
+	"gioui.org/app"
+	"gioui.org/op"
+	"gioui.org/widget/material"
 )
 
 func main() {
-	var exp string = parseArgs()
+	go func() {
+		window := new(app.Window)
+		err := run(window)
+		if err != nil {
+			log.Fatal(err)
+		}
+		os.Exit(0)
+	}()
 
-	ans, failed := calculations.Evaluate(exp)
-	if !failed {
-		fmt.Println("Answer is:", ans)
+	app.Main() //Boot up the UI window
+}
+
+func run(window *app.Window) error {
+	theme := material.NewTheme()
+	var ops op.Ops
+	ans, err := calculations.Evaluate("1+1")
+	if err {
+		log.Fatal("Calculation failed")
+	}
+
+	titleStr := fmt.Sprintf("1 + 1 = %f", ans)
+	for {
+		switch e := window.Event().(type) {
+		case app.DestroyEvent: //Window is closed
+			return e.Err
+		case app.FrameEvent: //Render cycle
+			//Graphics context
+			gtx := app.NewContext(&ops, e)
+
+			//Make title label
+			title := material.H1(theme, titleStr)
+
+			//Draw the title
+			title.Layout(gtx)
+
+			//Render
+			e.Frame(gtx.Ops)
+		}
 	}
 }
 
-func parseArgs() string {
-	if len(os.Args) != 2 || len(os.Args[1]) == 0 {
-		usage(os.Args[0])
-	}
+// func main() {
+// 	var exp string = parseArgs()
 
-	return strings.Join(os.Args[1:], "")
-}
+// 	ans, failed := calculations.Evaluate(exp)
+// 	if !failed {
+// 		fmt.Println("Answer is:", ans)
+// 	}
+// }
 
-func usage(progName string) {
-	fmt.Println("Usage: ", progName, "<expression>")
-	fmt.Println("\texpression - The expression to evaluate")
-	fmt.Println("\t\tSupported operators are: +, -, *, /, ^, ()")
-	os.Exit(1)
-}
+// func parseArgs() string {
+// 	if len(os.Args) != 2 || len(os.Args[1]) == 0 {
+// 		usage(os.Args[0])
+// 	}
+
+// 	return strings.Join(os.Args[1:], "")
+// }
+
+// func usage(progName string) {
+// 	fmt.Println("Usage: ", progName, "<expression>")
+// 	fmt.Println("\texpression - The expression to evaluate")
+// 	fmt.Println("\t\tSupported operators are: +, -, *, /, ^, ()")
+// 	os.Exit(1)
+// }
